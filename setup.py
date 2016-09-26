3f08779aac29b0522e2ae13933e94b945934b912
Tiago Ribeiro de Souza <tiago.astro@gmail.com>
2016-09-26 12:25:54 -0300

    Fix setup.py so it will install all libraries.

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name='t80s_reduce',
     version='0.0.1',
     packages=['t80s_reduce', 't80s_reduce.core', 't80s_reduce.util','t80s_reduce.t80s_process',
-              't80s_reduce.t80s_check_calibrations', 't80s_reduce.t80s_sort_night'],
+              't80s_reduce.t80s_check_calibrations', 't80s_reduce.t80s_sort_night', 't80s_reduce.t80s_gen_download',
+              't80s_reduce.t80s_sort_calibrations'],
     url='https://github.com/tribeiro/t80s_reduce/',
     license='BSD',
     author='Tiago Ribeiro de Souza',
@@ -13,7 +14,8 @@ setup(
     install_requires=['astropy',
                       'colorlog',
                       'ccdproc>=0.3.3',
-                      'fit_reduce'],
+                      'fit_reduce',
+                      'pyyaml'],
     scripts=['scripts/t80s_check_calibrations','scripts/t80s_gen_download','scripts/t80s_process',
              'scripts/t80s_sort_calibrations', 'scripts/t80s_sort_night']
 )
